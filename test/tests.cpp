93d6bd0976afc472d4820a99b708be4a22fd9bf3
DenisKabanov <dskabanov@edu.hse.ru>
2021-04-06 21:59:20 +0300

    what was wrong

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,24 +3,24 @@
 #include <string>
 #include "train.h"
 
-TEST(Test, test1) {
+TEST(Test1, test1) {
   Train train1(5);
   EXPECT_EQ(5, train1.sizeOfTrain());
 }
 
-TEST(Test, test2) {
+TEST(Test2, test2) {
   Train train2(5);
   train2.addCage(1);
   EXPECT_EQ(6, train2.sizeOfTrain());
 }
 
-TEST(Test, test3) {
+TEST(Test3, test3) {
   Train train3(0);
   train3.addCage(0);
   EXPECT_EQ(1, train3.sizeOfTrain());
 }
 
-TEST(Test, test4) {
+TEST(Test4, test4) {
   Train train4(5);
   for (int i = 0; i < 5; i++) {
     train4.addCage(0);
