7f5e8c6e10c73e2efbcd00b1f12401732cdab5c4
DenisKabanov <dskabanov@edu.hse.ru>
2021-04-06 21:49:01 +0300

    tests3

--- test/tests.cpp
+++ test/tests.cpp
@@ -16,28 +16,15 @@ TEST(Test, test2) {
 
 TEST(Test, test3) {
   Train train3(0);
-  train3.addCage(1);
-  EXPECT_EQ("l", train3.print());
+  train3.addCage(0);
+  EXPECT_EQ(1, train3.sizeOfTrain());
 }
 
 TEST(Test, test4) {
-  Train train4(0);
-  train4.addCage(0);
-  EXPECT_EQ("d", train4.print());
-}
-
-TEST(Test, test5) {
-  Train train5(0);
-  train5.addCage(1);
-  train5.sizeOfTrain();
-  EXPECT_EQ("d", train5.print());
-}
-
-TEST(Test, test6) {
-  Train train6(5);
+  Train train4(5);
   for (int i = 0; i < 5; i++) {
-    train6.addCage(0);
+    train4.addCage(0);
   }
-  EXPECT_EQ(10, train6.sizeOfTrain());
+  EXPECT_EQ(10, train4.sizeOfTrain());
 }
 
